67036f879bfb276c3b8a783ced1168074625a8e5
MNK29 <88828275+MNK29@users.noreply.github.com>
2022-04-22 17:46:27 +0530

    Add files via upload

---
+++ LetsChat-room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyARv3v6FtZ38K6EEVwJtwE2rCZELMIOmAk",
+    authDomain: "lets-chat-ee7d6.firebaseapp.com",
+    projectId: "lets-chat-ee7d6",
+    storageBucket: "lets-chat-ee7d6.appspot.com",
+    messagingSenderId: "471038277230",
+    appId: "1:471038277230:web:91d1f302ef348a110b587f"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
